87392c7b349d809993ca81d004a50b113f88ced6
emclaughlin42 <eric.mclaughlin@edu.sait.ca>
2023-04-10 00:11:08 -0600

    Initial commit

---
+++ Invoice.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeShop
+{
+    class Invoice
+    {
+        private const int BasePrice = 500;
+        private string name;
+        private string brandName;
+        private char brand;
+        private int tireSize;
+        private int metal;
+        private double metalPremium;
+        private double donation;
+
+        public Invoice()
+        {
+            name = "n/a";
+            brand = '-';
+            tireSize = 0;
+            metal = 0;
+            donation = 0;
+        }
+
+        //getter and setter of the name property
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                //check that the given string does not contain symbols, numbers or punctuation and is at least 2 characters long
+                if (value.Length >= 2)
+                {
+                    foreach (char index in value)
+                    {
+                        if (!Char.IsLetter(index) && index != ' ')
+                        {
+                            throw new Exception("Names cannot contain numbers, symbols or punctuation.");
+                        }
+                    }
+                }
+                else
+                {
+                    throw new Exception("Name must be 2 or more characters.");
+                }
+                name = value;
+            }
+        }
+
+        //getter and setter of the brand property, the brandName property is also set accordingly
+        public char Brand
+        {
+            get
+            {
+                return brand;
+            }
+            set
+            {
+                //if entered character is 'a', 'b', 'c' or 'd' set brand and brandName according to given value, if not throw exception to caller
+                switch (value)
+                {
+                    case 'a':
+                        brand = value;
+                        brandName = "Trek";
+                        break;
+
+                    case 'b':
+                        brand = value;
+                        brandName = "Giant";
+                        break;
+
+                    case 'c':
+                        brand = value;
+                        brandName = "Specialized";
+                        break;
+
+                    case 'd':
+                        brand = value;
+                        brandName = "Raleigh";
+                        break;
+                    default:
+                        throw new Exception("Value out of range.");
+                }
+            }
+        }
+
+        //getter and setter of the brandName property, the brand property is also set accordingly
+        public string BrandName
+        {
+            get
+            {
+                return brandName;
+            }
+            set
+            {
+                //if entered string is "Trek", "Giant", "Specialized" or "Raleigh" set brand and brandName according to given value, if not throw exception to caller
+                switch (value)
+                {
+                    case "Trek":
+                        brand = 'a';
+                        brandName = value;
+                        break;
+
+                    case "Giant":
+                        brand = 'b';
+                        brandName = value;
+                        break;
+
+                    case "Specialized":
+                        brand = 'c';
+                        brandName = value;
+                        break;
+
+                    case "Raleigh":
+                        brand = 'd';
+                        brandName = value;
+                        break;
+                    default:
+                        throw new Exception("String entered is not a recognized brand.");
+                }
+            }
+        }
+
+        //getter and setter of the tireSize property
+        public int TireSize
+        {
+            get
+            {
+                return tireSize;
+            }
+            set
+            {
+                //check if the given value is between 20 and 26
+                if (value >= 20 && value <= 26)
+                {
+                    tireSize = value;
+                }
+                else
+                {
+                    throw new Exception("Value out of range.");
+                }
+            }
+        }
+
+        //getter and setter of the metal property, metalPremium also gets accordingly
+        public int Metal
+        {
+            get
+            {
+                return metal;
+            }
+            set
+            {
+                //check if the given value is between 1 and 4 if it is set metal and metalPremium
+                switch (value)
+                {
+                    case 1:
+                        metal = value;
+                        metalPremium = 0;
+                        break;
+                    case 2:
+                        metal = value;
+                        metalPremium = 175;
+                        break;
+                    case 3:
+                        metal = value;
+                        metalPremium = 425;
+                        break;
+                    case 4:
+                        metal = value;
+                        metalPremium = 615;
+                        break;
+                    default:
+                        throw new Exception("Value out of range.");
+                }
+            }
+        }
+
+        //getter and setter of the metalPremium propery, also sets metal accordingly
+        public double MetalPremium
+        {
+            get
+            {
+                return metalPremium;
+            }
+            set
+            {
+                switch (value)
+                {
+                    case 0:
+                        metal = 1;
+                        metalPremium = 0;
+                        break;
+                    case 175:
+                        metal = 2;
+                        metalPremium = 175;
+                        break;
+                    case 425:
+                        metal = 3;
+                        metalPremium = 425;
+                        break;
+                    case 615:
+                        metal = 4;
+                        metalPremium = 615;
+                        break;
+                    default:
+                        throw new Exception("Value out of range.");
+                }
+            }
+        }
+
+
+        //getter and setter of the donation property
+        public double Donation
+        {
+            get
+            {
+                return donation;
+            }
+            set
+            {
+                //if donation is below 0 throw exception to caller
+                //allow 0 as an amount incase the menu option was accidentially selected as the donation is optional
+                if (value >= 0)
+                {
+                    donation = value;
+                }
+                else
+                {
+                    throw new Exception("Value out of range.");
+                }
+            }
+        }
+
+        //this method will display the packing slip of the invoice
+        public void Display()
+        {
+            //initialize variables
+            double subtotal, gst, total, tireSizePremium;
+
+            //calculate tire size premium and metal premiums
+            tireSizePremium = tireSize * 17.5;
+
+
+            //display receipt header and customer name
+            Console.WriteLine("Invoice and Packing Slip");
+            Console.WriteLine($"Customer: {name}");
+
+            //calcualte subtotal, gst and total
+            subtotal = BasePrice + tireSizePremium + metalPremium;
+            gst = subtotal * 0.05;
+            total = subtotal + gst + donation;
+            subtotal = Math.Round(subtotal, 2);
+            gst = Math.Round(gst, 2);
+            total = Math.Round(total, 2);
+
+
+            //display their receipt with proper formatting
+            Console.WriteLine($"{"Brand",-24} {brandName,15}");
+            Console.WriteLine($"{"Base Price:",-24} {BasePrice,15:F2}");
+            Console.WriteLine($"{"Tire Size Premium:",-24} {tireSizePremium,15:F2}");
+            Console.WriteLine($"{"Metal Selection Premium:",-24} {metalPremium,15:F2}");
+            Console.WriteLine($"{"-------",40}");
+            Console.WriteLine($"{"Subtotal:",-24} {subtotal,15:F2}");
+            Console.WriteLine($"{"GST:",-24} {gst,15:F2}");
+            Console.WriteLine($"{"=======",40}");
+
+            //display donation amount only if they made one
+            if (donation > 0)
+            {
+                Console.WriteLine($"{"Donation to Green Earth:",-24} {donation,15:F2}");
+            }
+            //display their total
+            Console.WriteLine($"{"Total:",-24} {total,15:F2}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,452 @@
+﻿/* Author: Eric McLaughlin
+ * Last Date Modified: April 8, 2020
+ * 
+ * Purpose: Allow the user to enter a number of invoices into the program and display their current invoice, save their invoice to a list and save that list of invoices to a file.
+ *          Also to read a list of invoices from a file. And to display the list of invoices in a table.
+ * 
+ */
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace BikeShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //initalize objects and variables
+            List<Invoice> invoices = new List<Invoice>();
+            Invoice currentInvoice = new Invoice();
+            int menuChoice;
+
+
+            //display header
+            Console.WriteLine("The Right Bike Shop");
+            Console.WriteLine("*******************");
+
+
+            //loop through until user selects option to exit program
+            do
+            {
+                //display menu options
+                menuChoice = getMenuChoice();
+
+                if (menuChoice == 1)
+                {
+                    //prompt user for their name and store it
+                    EnterName(currentInvoice);
+                    Console.WriteLine();
+                }
+                else if (menuChoice == 2)
+                {
+                    //display a list of available brands, prompt the user for the choice and store it
+                    ChooseBrand(currentInvoice);
+                    Console.WriteLine();
+                }
+                else if (menuChoice == 3)
+                {
+                    //display the availbe tire sizes, prompt the user for their choice and store it
+                    ChooseTireSize(currentInvoice);
+                    Console.WriteLine();
+                }
+                else if (menuChoice == 4)
+                {
+                    //display a list of available metals, prompt the user for their choice and store it
+                    ChooseCompositeValue(currentInvoice);
+                    Console.WriteLine();
+                }
+                else if (menuChoice == 5)
+                {
+                    //prompt the user to enter a donation amount and store it
+                    Donation(currentInvoice);
+                    Console.WriteLine();
+                }
+                else if (menuChoice == 6)
+                {
+                    //display the packing slip for the current invoice
+                    Console.WriteLine();
+                    currentInvoice.Display();
+                    Console.WriteLine();
+                }
+                else if (menuChoice == 7)
+                {
+                    //save the current invoice to a list a create a new invoice
+                    if (!(currentInvoice.Name == "n/a" || currentInvoice.Brand == '-' || currentInvoice.TireSize == 0 || currentInvoice.Metal == 0))
+                    {
+                        invoices.Add(currentInvoice);
+                        currentInvoice = new Invoice();
+                    }
+                    else
+                    {
+                        Console.Write("Invoice could not be added, one or more fields has not been set.");
+                    }
+                    Console.WriteLine();
+                }
+                else if (menuChoice == 8)
+                {
+                    //prompt the user for a file path and read in a list of invoices from it
+                    invoices = ReadFromFile();
+                    Console.WriteLine();
+                }
+                else if (menuChoice == 9)
+                {
+                    //prompt the user for a file path and write the list of invoices to it
+                    WriteToFile(invoices);
+                    Console.WriteLine();
+                }
+                else if (menuChoice == 10)
+                {
+                    //display the list of invoices in a table
+                    DisplayAllInvoices(invoices);
+                    Console.WriteLine();
+                }
+                //exit program is 11 is chosen
+            } while (menuChoice != 11);
+        }
+
+        static public int getMenuChoice()
+        {
+            int menuChoice;
+
+            //display menu options
+            Console.WriteLine("Please select one of the following menu options:");
+            Console.WriteLine("1. Enter Name");
+            Console.WriteLine("2. Select Brand");
+            Console.WriteLine("3. Select Tire Size");
+            Console.WriteLine("4. Select Metal");
+            Console.WriteLine("5. Add Donation");
+            Console.WriteLine("6. Display Invoice");
+            Console.WriteLine("7. Save Invoice");
+            Console.WriteLine("8. Read Invoices from File");
+            Console.WriteLine("9. Write Invoics to File");
+            Console.WriteLine("10. Display All Invoices");
+            Console.WriteLine("11. Exit");
+
+            //get int between 1 and 8
+            menuChoice = getSafeInt("Enter your selection (1-11): ", 1, 11);
+
+            return menuChoice;
+        }
+
+        //get a valid int from the user
+        static public int getSafeInt(string prompt)
+        {
+            int safeInt = 0;
+            bool validData = false;
+
+            //loop through until valid data is entered
+            do
+            {
+                //prompt user user given prompt
+                Console.Write(prompt);
+                try
+                {
+                    safeInt = int.Parse(Console.ReadLine());
+                    validData = true;
+                }
+                //handle any exception
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Invalid input, try again");
+                }
+            } while (!validData);
+
+            return safeInt;
+        }
+
+        //get a valid int between min and max values from the user
+        static public int getSafeInt(string prompt, int min, int max)
+        {
+            int safeInt;
+            bool validData = false;
+
+            //loop through until valid data is entered
+            do
+            {
+                //get valid int from getSafeInt
+                safeInt = getSafeInt(prompt);
+
+                //check if it is in range if not tell user if it is exit loop
+                if (safeInt >= min && safeInt <= max)
+                {
+                    validData = true;
+                }
+                else
+                {
+                    Console.WriteLine("Input out of range, try again");
+                }
+            } while (!validData);
+
+            return safeInt;
+        }
+
+
+        //get a valid double from the user
+        static public double getSafeDouble(string prompt)
+        {
+            double safeDouble = 0;
+            bool validData = false;
+
+            //loop through until valid data is entered
+            do
+            {
+                //prompt user using given prompt
+                Console.Write(prompt);
+                try
+                {
+                    safeDouble = double.Parse(Console.ReadLine());
+                    validData = true;
+                }
+                //handle any exception
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Invalid input, try again");
+                }
+            } while (!validData);
+
+            return safeDouble;
+        }
+
+        //get safe double within min and max value from user
+        static public double getSafeDouble(string prompt, double min, double max)
+        {
+            double safeDouble;
+            bool validData = false;
+
+            //loop through until valid data is entered
+            do
+            {
+                safeDouble = getSafeDouble(prompt);
+
+                //check if user input is within range if it is exit loop if not tell user and continue loop
+                if (safeDouble >= min && safeDouble <= max)
+                {
+                    validData = true;
+                }
+                else
+                {
+                    Console.WriteLine("Input out of range, try again");
+                }
+            } while (!validData);
+
+            return safeDouble;
+        }
+
+        //promp the user to enter a name and store it in a given Invoice object
+        static void EnterName(Invoice invoice)
+        {
+            bool validData = false;
+            //loop through until a valid name is entered
+            do
+            {
+                //prompt user for a name
+                Console.Write("Enter a name: ");
+                try
+                {
+                    //store the name
+                    invoice.Name = Console.ReadLine();
+                    validData = true;
+                }
+                catch (Exception ex)
+                {
+                    //if an exception is thrown display the message
+                    Console.WriteLine($"Error: {ex.Message} Try again");
+                    Console.WriteLine();
+                }
+            } while (!validData);
+        }
+
+        //display available brands prompt users for their selection and return their choice in a given Invoice object
+        static void ChooseBrand(Invoice invoice)
+        {
+            bool validData = false;
+
+            //display available brands
+            Console.WriteLine("Available brands");
+            Console.WriteLine(" a) Trek");
+            Console.WriteLine(" b) Giant");
+            Console.WriteLine(" c) Specialized");
+            Console.WriteLine(" d) Raleigh");
+
+            //loop through until valid data is entered
+            do
+            {
+                Console.Write("Enter your selection (a, b, c, d): ");
+                try
+                {
+                    invoice.Brand = Console.ReadLine()[0];
+                    validData = true;
+                }
+                //if an exception is thrown display the message
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message} Try again.");
+                    Console.WriteLine();
+                }
+            } while (!validData);
+        }
+
+        //display available tire sizes and return their selection
+        static void ChooseTireSize(Invoice invoice)
+        {
+            //prompt user for their prefered tire size and store it
+            invoice.TireSize = getSafeInt("Select a tire size (20-26) @ $17.50 per inch: ", 20, 26);
+        }
+
+        //display available metals and prompt users for their selection and return the premium
+        static void ChooseCompositeValue(Invoice invoice)
+        {
+            //display available metal alloys
+            Console.WriteLine("Available metal alloys");
+            Console.WriteLine(" 1) Steel ($0)");
+            Console.WriteLine(" 2) Aluminum ($175)");
+            Console.WriteLine(" 3) Titanium ($425)");
+            Console.WriteLine(" 4) Carbon Fibre ($615)");
+
+            //prompt user for selection and store it
+            invoice.Metal = getSafeInt("Enter your selection (1-4): ", 1, 4);
+
+
+        }
+
+        //prompt users for a donation amount and set the Invoice object variable to that amount if it is greater than or equal to 0
+        static void Donation(Invoice invoice)
+        {
+            bool validData = false;
+
+            do
+            {
+                try
+                {
+                    //ask for the amount and store it
+                    invoice.Donation = getSafeDouble("Enter amount: ");
+                    validData = true;
+                }
+                //if an exception is thrown display the message
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message} Try again.");
+                }
+
+            } while (!validData);
+        }
+
+        //method will read client data from a file selected by the user into a list of Invoice objects
+        static List<Invoice> ReadFromFile()
+        {
+            //initialize objects and variables
+            List<Invoice> invoices = new List<Invoice>();
+            Invoice current;
+            StreamReader file;
+            string[] lineArray;
+            string filePath;
+            string line;
+            int lineNum = 0;
+
+            //prompt user for file path
+            Console.Write("Enter the file to read from: ");
+            filePath = Console.ReadLine();
+
+            try
+            {
+                //open file
+                file = new StreamReader(filePath);
+
+                //go through file line by line
+                while ((line = file.ReadLine()) != null)
+                {
+                    //keep track of current line number
+                    lineNum++;
+                    //split each line into an array of strings and create a new Invoice object with the values read from the file
+                    current = new Invoice();
+                    lineArray = line.Split(", ");
+
+                    //store the values into the current Invoice object
+                    current.Name = lineArray[0];
+                    current.BrandName = lineArray[1];
+                    current.TireSize = int.Parse(lineArray[2]);
+                    current.MetalPremium = double.Parse(lineArray[3]);
+                    current.Donation = double.Parse(lineArray[4]);
+
+                    //add new Invoice object to the list
+                    invoices.Add(current);
+                }
+
+                //close the file
+                file.Close();
+            }
+            //if the file cannot be opened or read from display the message
+            catch (Exception ex)
+            {
+                Console.Write($"Error reading from file {filePath} with exception: {ex.Message}");
+                //if the file opened and the error was from the values being read display the line the error was found on
+                if (lineNum > 0)
+                {
+                    Console.WriteLine($" on line {lineNum}");
+                }
+                else
+                {
+                    Console.WriteLine();
+                }
+            }
+
+            //return the list of Invoices
+            return invoices;
+        }
+
+        //this method will write the list of invoices to a file
+        static void WriteToFile(List<Invoice> invoices)
+        {
+            StreamWriter file;
+            String filePath;
+
+            //get the path where the file will be stored
+            Console.Write("Enter the file to write to: ");
+            filePath = Console.ReadLine();
+
+            try
+            {
+                //open the file
+                file = new StreamWriter(filePath);
+
+                //for every Invoice object in the list
+                foreach (Invoice invoice in invoices)
+                {
+                    //write each entry of the Invoice objects to the file in the format of "{Name}, {Brand}, {TireSize}, {MetalPremium}, {Donation}"
+                    file.WriteLine($"{invoice.Name}, {invoice.BrandName}, {invoice.TireSize}, {invoice.MetalPremium}, {invoice.Donation}");
+                }
+
+                //close the file
+                file.Close();
+            }
+            //if an exception is thrown display the message
+            catch (Exception ex)
+            {
+                Console.Write($"Error writing to file {filePath} with exception {ex.Message}");
+            }
+        }
+
+        //this method will display the list of Invoice objects in a table if it is not empty
+        static void DisplayAllInvoices(List<Invoice> invoices)
+        {
+            if (invoices.Count > 0)
+            {
+                Console.WriteLine($"{"Client",-20}{"Brand",-15}{"Tires",-10}{"Metal",10}{"Donation",12}");
+                Console.WriteLine("-------------------------------------------------------------------");
+                foreach (Invoice invoice in invoices)
+                {
+                    Console.WriteLine($"{invoice.Name,-20}{invoice.BrandName,-15}{invoice.TireSize,-10}{invoice.MetalPremium,10:0.00}{invoice.Donation,12:0.00}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("There are no saved invoices to be listed.");
+            }
+        }
+    }
+
+
+    
+}
